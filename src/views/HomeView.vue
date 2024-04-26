ade299544481b56fd4a2435f2b94a71af4b69208
Fagner Araujo <ifgr.fagner@gmail.com>
2024-04-26 20:09:37 -0300

    chore: first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="HomeView">
+    <Logo class="logo"/>
+  </main>
+</template>
+
+<script setup>
+import Logo from '@/components/Logo.vue';
+</script>
+
+<style>
+.HomeView {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+
+.logo {
+  height: 70vh;
+}
+</style>
