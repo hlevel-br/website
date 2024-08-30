55a0cdc04534d3a4c270ffc18c6bcbb1abad867e
Fagner Araujo <fagner.araujo@azion.com>
2024-08-30 18:41:16 -0300

    chore: turn into uborn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,52 @@
 <template>
   <main class="HomeView">
-    <Logo class="logo"/>
+    <main class="main">
+      <Logo class="logo" />
+    </main>
+    <section class="footer">
+      <p class="name">
+        2024 - <strong>uBorn</strong> :: Engenharia de Software
+      </p>
+      <a href="mailto:contato@uborn.com.br" class="contact"
+        >contato@uborn.com.br</a
+      >
+      <p class="companyid">CNPJ: <strong>51.975.973/0001-87</strong></p>
+    </section>
   </main>
 </template>
 
 <script setup>
-import Logo from '@/components/Logo.vue';
+import Logo from "@/components/Logo.vue";
 </script>
 
 <style>
 .HomeView {
   display: flex;
+  flex-direction: column;
   height: 100vh;
-  justify-content: center;
   align-items: center;
 }
 
-.logo {
+.main {
+  display: flex;
+  align-items: center;
+  flex: 1 1 auto;
+}
+.main > .logo {
   height: 70vh;
+}
+
+.footer {
+  flex: 0 0 80px;
+  text-align: center;
+}
+.footer strong {
+  font-weight: 700;
+}
+.footer > .contact {
+  color: var(--color-text);
+}
+.footer > .companyid {
+  font-size: 0.8rem;
 }
 </style>
